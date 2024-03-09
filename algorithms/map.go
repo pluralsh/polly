467889b6197f0dcbe9ee6652269da15b26d7df23
michaeljguarino <mguarino46@gmail.com>
2024-03-08 20:40:33 -0500

    Make map merge variadic
    Can help with cases where you actually need to merge 3+ maps which does happen with helm-y stuff.

--- algorithms/map.go
+++ algorithms/map.go
@@ -1,6 +1,15 @@
 package algorithms
 
-func Merge(m1, m2 map[string]interface{}) map[string]interface{} {
+func Merge(maps ...map[string]interface{}) map[string]interface{} {
+	res := maps[0]
+	for _, m := range maps[1:] {
+		res = deepMerge(res, m)
+	}
+
+	return res
+}
+
+func deepMerge(m1, m2 map[string]interface{}) map[string]interface{} {
 	// lifted from helm's merge code
 	out := make(map[string]interface{}, len(m1))
 	for k, v := range m1 {
@@ -11,7 +20,7 @@ func Merge(m1, m2 map[string]interface{}) map[string]interface{} {
 		if v, ok := v.(map[string]interface{}); ok {
 			if bv, ok := out[k]; ok {
 				if bv, ok := bv.(map[string]interface{}); ok {
-					out[k] = Merge(bv, v)
+					out[k] = deepMerge(bv, v)
 					continue
 				}
 			}
