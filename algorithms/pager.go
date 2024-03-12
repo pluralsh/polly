3e509ddf50dc47cb5d1fcc5901337d4d29f9982a
Lukasz Zajaczkowski <zreigz@gmail.com>
2024-03-12 16:56:34 +0100

    improve NextPage

--- algorithms/pager.go
+++ algorithms/pager.go
@@ -33,17 +33,15 @@ func NewPager[T any](pageSize int64, fetchPage FetchPageFunc[T]) *Pager[T] {
 
 func (p *Pager[T]) NextPage() ([]T, error) {
 	var list []T
-
-	if p.err != nil {
-		return nil, p.err
-	}
-
+	
 	if !p.pageInfo.HasNext {
 		return nil, nil
 	}
 
 	list, p.pageInfo, p.err = p.fetchPage(p.pageInfo.After, p.pageInfo.PageSize)
-
+	if p.err != nil {
+		return nil, p.err
+	}
 	return list, nil
 }
 
