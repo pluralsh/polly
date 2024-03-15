d8aadc71d24bcce388d2112aaa1dd1fd3dd5e4ab
Lukasz Zajaczkowski <zreigz@gmail.com>
2024-03-15 16:25:28 +0100

    change name

--- algorithms/map.go
+++ algorithms/map.go
@@ -1,6 +1,6 @@
 package algorithms
 
-func MapToSlice[K comparable, V any](m map[K]V) []V {
+func MapValues[K comparable, V any](m map[K]V) []V {
 	s := make([]V, 0, len(m))
 	for _, v := range m {
 		s = append(s, v)
