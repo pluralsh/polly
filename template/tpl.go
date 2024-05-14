906161ff2702bbb00fbb438d9cebfaa4a3c11857
seemywings <kevin.jayne@icloud.com>
2024-05-14 12:34:50 -0400

    map include function for .tpl rendering

--- template/tpl.go
+++ template/tpl.go
@@ -7,13 +7,40 @@ import (
 	"github.com/Masterminds/sprig/v3"
 )
 
+// include function to include other templates
+func include(name string, data interface{}, tpl *template.Template) (string, error) {
+	var buf bytes.Buffer
+	err := tpl.ExecuteTemplate(&buf, name, data)
+	if err != nil {
+		return "", err
+	}
+	return buf.String(), nil
+}
+
+// RenderTpl renders the given Helm .tpl template with the provided bindings.
 func RenderTpl(input []byte, bindings map[string]interface{}) ([]byte, error) {
-	tpl, err := template.New("gotpl").Funcs(sprig.TxtFuncMap()).Parse(string(input))
+	// Create a new template and add the sprig functions and the include function.
+	tpl := template.New("gotpl")
+	tpl.Funcs(sprig.TxtFuncMap()).Funcs(template.FuncMap{
+		"include": func(name string, data interface{}) (string, error) {
+			return include(name, data, tpl)
+		},
+	})
+
+	// Parse the input template.
+	tpl, err := tpl.Parse(string(input))
 	if err != nil {
 		return nil, err
 	}
 
+	// Create a buffer to hold the rendered template.
 	var buffer bytes.Buffer
+
+	// Execute the template with the bindings and write to the buffer.
 	err = tpl.Execute(&buffer, bindings)
-	return buffer.Bytes(), err
+	if err != nil {
+		return nil, err
+	}
+
+	return buffer.Bytes(), nil
 }
