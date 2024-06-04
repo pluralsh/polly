e6a4ce0b055b339bf645f6a00750ff29b552ed04
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-06-04 12:11:03 +0200

    backfill sprig functions in liquid templating

--- template/liquid.go
+++ template/liquid.go
@@ -1,38 +1,31 @@
 package template
 
 import (
-	"strings"
-
 	"github.com/Masterminds/sprig/v3"
 	"github.com/osteele/liquid"
 )
 
 var (
-	liquidEngine   = liquid.NewEngine()
-	sprigFunctions = map[string]string{
+	liquidEngine         = liquid.NewEngine()
+	sprigFunctionNameMap = map[string]string{
 		"toJson":        "to_json",
 		"fromJson":      "from_json",
-		"b64enc":        "b64enc",
-		"b64dec":        "b64dec",
 		"semverCompare": "semver_compare",
 		"sha256sum":     "sha26sum",
-		"quote":         "quote",
-		"squote":        "squote",
-		"replace":       "replace",
-		"coalesce":      "coalesce",
 	}
 )
 
 func init() {
 	fncs := sprig.TxtFuncMap()
-	for key, name := range sprigFunctions {
+
+	for name, fnc := range fncs {
+		liquidEngine.RegisterFilter(name, fnc)
+	}
+
+	for key, name := range sprigFunctionNameMap {
 		liquidEngine.RegisterFilter(name, fncs[key])
 	}
-	liquidEngine.RegisterFilter("indent", indent)
-	liquidEngine.RegisterFilter("nindent", nindent)
-	liquidEngine.RegisterFilter("replace", strings.ReplaceAll)
 
-	liquidEngine.RegisterFilter("default", dfault)
 	liquidEngine.RegisterFilter("ternary", ternary)
 }
 
