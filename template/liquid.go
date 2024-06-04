9d660189c21487fc3b2e30fe456cb1e13280c589
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-06-04 16:54:40 +0200

    exclude funcs without params

--- template/liquid.go
+++ template/liquid.go
@@ -3,11 +3,13 @@ package template
 import (
 	"github.com/Masterminds/sprig/v3"
 	"github.com/osteele/liquid"
+	"github.com/pluralsh/polly/algorithms"
 )
 
 var (
-	liquidEngine         = liquid.NewEngine()
-	sprigFunctionNameMap = map[string]string{
+	liquidEngine           = liquid.NewEngine()
+	excludedSprigFunctions = []string{"hello", "now", "uuidv4"}
+	sprigFunctionNameMap   = map[string]string{
 		"toJson":        "to_json",
 		"fromJson":      "from_json",
 		"semverCompare": "semver_compare",
@@ -19,7 +21,9 @@ func init() {
 	fncs := sprig.TxtFuncMap()
 
 	for name, fnc := range fncs {
-		liquidEngine.RegisterFilter(name, fnc)
+		if algorithms.Index(excludedSprigFunctions, func(s string) bool { return s == name }) < 0 {
+			liquidEngine.RegisterFilter(name, fnc)
+		}
 	}
 
 	for key, name := range sprigFunctionNameMap {
