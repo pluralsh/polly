3c6f630b50f5d8cba2da9d9e870133a1d7553d50
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-09-19 14:11:00 +0200

    add method to return registered filters info

--- template/liquid.go
+++ template/liquid.go
@@ -1,12 +1,22 @@
 package template
 
 import (
+	"reflect"
+	"runtime"
 	"strings"
 
 	"github.com/Masterminds/sprig/v3"
 	"github.com/osteele/liquid"
 	"github.com/samber/lo"
 )
+
+type FilterFunction struct {
+	Name            string   `json:"name"`
+	Aliases         []string `json:"aliases,omitempty"`
+	FunctionPath    string   `json:"functionPath,omitempty"`
+	FunctionPackage string   `json:"functionPackage,omitempty"`
+	FunctionName    string   `json:"functionName,omitempty"`
+}
 
 var (
 	liquidEngine = liquid.NewEngine()
@@ -15,11 +25,11 @@ var (
 	excludedSprigFunctions = []string{"hello", "now", "uuidv4"}
 
 	// sprigFunctionNameAliases contains additional aliases for Sprig functions.
-	sprigFunctionNameAliases = map[string]string{
-		"toJson":        "to_json",
-		"fromJson":      "from_json",
-		"semverCompare": "semver_compare",
-		"sha256sum":     "sha26sum",
+	sprigFunctionNameAliases = map[string][]string{
+		"toJson":        {"to_json"},
+		"fromJson":      {"from_json"},
+		"semverCompare": {"semver_compare"},
+		"sha256sum":     {"sha26sum"},
 	}
 
 	// internalFunctions to register. These will override Sprig functions if same names are used.
@@ -30,29 +40,52 @@ var (
 		"default": dfault,
 		"ternary": ternary,
 	}
+
+	// registeredFunctions contains information about all registered template functions.
+	registeredFunctions = map[string]FilterFunction{}
 )
 
 func init() {
-	fncs := sprig.TxtFuncMap()
-
-	excludedFunctions := excludedSprigFunctions
-	for k := range internalFunctions {
-		excludedFunctions = append(excludedFunctions, k)
-	}
-
-	for name, fnc := range fncs {
-		if !lo.Contains(excludedFunctions, name) {
-			liquidEngine.RegisterFilter(name, fnc)
+	sprigFunctions := sprig.TxtFuncMap()
+	for name, fnc := range sprigFunctions {
+		_, hasInternalFunctionNameConflict := internalFunctions[name]
+		if !lo.Contains(excludedSprigFunctions, name) && !hasInternalFunctionNameConflict {
+			registerFilter(name, sprigFunctionNameAliases[name], fnc)
 		}
 	}
 
-	for key, name := range sprigFunctionNameAliases {
-		liquidEngine.RegisterFilter(name, fncs[key])
+	for name, fnc := range internalFunctions {
+		registerFilter(name, nil, fnc)
+	}
+}
+
+func registerFilter(name string, aliases []string, fn any) {
+	liquidEngine.RegisterFilter(name, fn)
+
+	for _, alias := range aliases {
+		liquidEngine.RegisterFilter(alias, fn)
 	}
 
-	for name, fnc := range internalFunctions {
-		liquidEngine.RegisterFilter(name, fnc)
+	fnPath := runtime.FuncForPC(reflect.ValueOf(fn).Pointer()).Name()
+	lastDot := strings.LastIndexByte(fnPath, '.')
+	fnPackage := ""
+	fnName := ""
+	if lastDot > 1 {
+		fnPackage = fnPath[:lastDot]
+		fnName = fnPath[lastDot+1:]
 	}
+
+	registeredFunctions[name] = FilterFunction{
+		Name:            name,
+		Aliases:         aliases,
+		FunctionPath:    fnPath,
+		FunctionPackage: fnPackage,
+		FunctionName:    fnName,
+	}
+}
+
+func RegisteredFilters() map[string]FilterFunction {
+	return registeredFunctions
 }
 
 func RenderLiquid(input []byte, bindings map[string]interface{}) ([]byte, error) {
