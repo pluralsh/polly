92362fd70ebcc8e1b4bc0d64d44edd70881677ec
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-09-23 14:46:35 +0200

    extract functions

---
+++ internal/template/docs.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"io"
+	"os"
+	"sort"
+	"strings"
+	"text/template"
+
+	"github.com/Masterminds/sprig/v3"
+	_ "github.com/pluralsh/polly/template"
+	tmpl "github.com/pluralsh/polly/template"
+	"github.com/samber/lo"
+)
+
+const (
+	docsPath     = "docs/liquid-filters.md"
+	docsTemplate = `...
+
+{{ range . }}
+##  {{ .Name }}
+{{ .Description }}
+
+{{ if .Aliases }}Aliases: {{ .Aliases | join ", " }}{{ end }}
+
+Implementation: {{ .Implementation }}
+{{ end }}`
+)
+
+func main() {
+	f, err := os.Create(docsPath)
+	if err != nil {
+		panic(err)
+	}
+
+	if err = generateFilterDocs(f, getRegisteredFilters()); err != nil {
+		panic(err)
+	}
+}
+
+func getRegisteredFilters() []tmpl.FilterFunction {
+	filters := lo.Values(tmpl.RegisteredFilters())
+	sort.Slice(filters, func(i, j int) bool {
+		return strings.Compare(filters[i].Name, filters[j].Name) < 0
+	})
+
+	return filters
+}
+
+func generateFilterDocs(writer io.Writer, filters []tmpl.FilterFunction) error {
+	t := template.Must(template.New("").Funcs(sprig.TxtFuncMap()).Parse(docsTemplate))
+	return t.Execute(writer, filters)
+}
