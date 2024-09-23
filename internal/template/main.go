9596d4d737b2f3d678e1a4f14b5fa784f9cb2173
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-09-23 11:42:31 +0200

    add more docs

--- internal/template/main.go
+++ internal/template/main.go
@@ -1,18 +1,38 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
+	"os"
+	"sort"
+	"strings"
+	"text/template"
 
-	"github.com/pluralsh/polly/template"
+	"github.com/Masterminds/sprig/v3"
 	_ "github.com/pluralsh/polly/template"
+	tmpl "github.com/pluralsh/polly/template"
+	"github.com/samber/lo"
+)
+
+const (
+	templ = `...
+
+{{ range . }}
+##  {{ .Name }}
+{{ .Description }}
+
+{{ if .Aliases }}Aliases: {{ .Aliases | join ", " }}{{ end }}
+Implementation: {{ .Implementation }}
+{{ end }}`
 )
 
 func main() {
-	marshal, err := json.MarshalIndent(template.RegisteredFilters(), "", "\t")
+	filters := lo.Values(tmpl.RegisteredFilters())
+	sort.Slice(filters, func(i, j int) bool {
+		return strings.Compare(filters[i].Name, filters[j].Name) < 0
+	})
+
+	t := template.Must(template.New("").Funcs(sprig.TxtFuncMap()).Parse(templ))
+	err := t.Execute(os.Stdout, filters)
 	if err != nil {
 		panic(err)
 	}
-
-	fmt.Println(string(marshal))
 }
