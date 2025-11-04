82839a6d878bd3d78edf1bc3385f3b61394c9403
Lukasz Zajaczkowski <zreigz@gmail.com>
2025-11-04 15:28:11 +0100

    correctly initialize the safe standard libraries (#62)

--- luautils/lua.go
+++ luautils/lua.go
@@ -15,24 +15,12 @@ func NewLuaState(path string) *lua.LState {
 	})
 
 	// Load only safe standard libraries
-	for _, pair := range []struct {
-		n string
-		f lua.LGFunction
-	}{
-		{lua.LoadLibName, lua.OpenPackage},
-		{lua.BaseLibName, lua.OpenBase},
-		{lua.TabLibName, lua.OpenTable},
-		{lua.StringLibName, lua.OpenString},
-		{lua.MathLibName, lua.OpenMath},
-	} {
-		if err := L.CallByParam(lua.P{
-			Fn:      L.NewFunction(pair.f),
-			NRet:    0,
-			Protect: true,
-		}, lua.LString(pair.n)); err != nil {
-			panic(err)
-		}
-	}
+	lua.OpenBase(L)
+	lua.OpenString(L)
+	lua.OpenTable(L)
+	lua.OpenMath(L)
+	lua.OpenPackage(L)
+
 	p := &Processor{BasePath: path}
 	// Register custom modules
 	RegisterEncodingModule(p, L)
